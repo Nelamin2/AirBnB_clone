cc84133f129d05eccb404d3493045405305f549a
Nelamin2 <nidalelamin@gmail.com>
2024-02-11 15:19:39 -0800

    fixed storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,36 +11,33 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
+
 class FileStorage:
     """Represent the filestorage.
     Attributes:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
     """
-    
 
     __file_path = "file.json"
     __objects = {}
-
 
     def all(self):
         """Retrieves allobjects from storage"""
         return FileStorage.__objects
 
-
     def new(self, obj):
         """Set in __objects obj with key <obj_class_name>.id"""
-        FileStorage.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
-
+        FileStorage.__objects["{}.{}"
+                              .format(obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
         tempdic = FileStorage.__objects
-        newobjdict = {obj: tempdic[obj].to_dict() for obj in temp.keys()}
+        newobjdict = {obj: tempdic[obj].to_dict() for obj in tempdic.keys()}
         with open(FileStorage.__file_path, "w") as f:
-            json.dump(newobjdict, f)    
+            json.dump(newobjdict, f)
 
-    
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
